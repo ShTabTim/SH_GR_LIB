8b60fc2cb7a3f1f4333eb7f8146f66b7f5e05175
ShTabTim <85260208+ShTabTim@users.noreply.github.com>
2022-09-24 20:20:33 +0300

    examples

--- main.cpp
+++ main.cpp
@@ -17,20 +17,18 @@ float comp_fps() {
 
 void rend(shwin* wnd) {
 	hd.ring(rand()%hd.width, rand()%hd.height, rand()%64, 0xFF000000+0x00FFFF00&(rand()));
-
-	hd.line(512, 512, rand() % hd.width - 512, rand() % hd.height - 512, 0xFF000000 + 0x00FFFF00 & (rand()));
+	hd.line(hd.width * 0.5f, hd.height * 0.5f, rand() % hd.width - hd.width * 0.5f, rand() % hd.height - hd.height * 0.5f, 0xFF000000 + 0x00FFFF00 & rand());
 
 	StretchDIBits(wnd->hdc, 0, 0, wnd->width, wnd->height, 0, 0, hd.width, hd.height, hd.handle, &wnd->buf_info, DIB_RGB_COLORS, SRCCOPY);
 
-	wchar_t s[64];
-	swprintf_s(s, 64, L"%5.3f fps", comp_fps());
-	TextOut(wnd->hdc, 0, 0, s, 64);
+	std::wstring s = L"Speed: " + std::to_wstring(comp_fps()) + L" fps";
+	TextOut(wnd->hdc, 0, 0, s.c_str(), s.length());
 }
 
 int WinMain(HINSTANCE hinst, HINSTANCE prev_hinst, LPSTR cmd_line, int show_cmd) {
-	hd.init(1024, 1024);
 
-	wnd.init(hinst, L"Graphics test", rend, 1024, 1024);
+	hd.init(480, 320);
+	wnd.init(hinst, L"Graphics test", rend, 480, 320);
 	wnd.init_draw_area(hd.width, hd.height);
 	wnd.show_window(show_cmd);
 
