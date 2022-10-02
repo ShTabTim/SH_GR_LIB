9121af6dba97850e269c5855afaba5d55cf77f66
ShTabTim <85260208+ShTabTim@users.noreply.github.com>
2022-10-02 13:10:40 +0300

    hh

--- main.cpp
+++ main.cpp
@@ -18,26 +18,27 @@ unsigned long comp_mspf() {
 
 void rend(shwin* wnd) {
 	hd.clear(0xFF00000F);
-	for (uint32_t u(256); u--;) {
-		hd.circ(rand() % hd.width, rand() % hd.height, 32 + rand() % 32, 0xFF000000 + 0x00FFFF00 & (rand()));
-		hd.rect(rand() % hd.width, rand() % hd.height, 32 + rand() % 32, 32 + rand() % 32, 0xFF000000 + 0x00FFFF00 & (rand()));
+	for (uint32_t u(64); u--;) {
+		hd.circ(rand() % hd.width, rand() % hd.height, 64 + rand() % 64, 0xFF000000 + 0x00FFFF00 & (rand()));
+		hd.rect(rand() % hd.width, rand() % hd.height, 64 + rand() % 64, 64 + rand() % 64, 0xFF000000 + 0x00FFFF00 & (rand()));
 		uint32_t x = rand() % hd.width;
 		uint32_t y = rand() % hd.height;
-		hd.rect2p(x, y, x + rand() % 64, y + rand() % 64, 0xFF000000 + 0x00FFFF00 & (rand()));
+		hd.rect2p(x, y, x + rand() % 128, y + rand() % 128, 0xFF000000 + 0x00FFFF00 & (rand()));
 		hd.ring(rand() % hd.width, rand() % hd.height, 64 + rand() % 64, 0xFF000000 + 0x00FFFF00 & (rand()));
-		hd.line2p(hd.width >> 1, hd.height >> 1, rand() % hd.width, rand() % hd.height, 0xFF000000 + 0x00FFFF00 & rand());
+		hd.line(hd.width >> 1, hd.height >> 1, (rand() % hd.width) - (hd.width >> 1), (rand() % hd.height) - (hd.height >> 1), 0xFF000000 + 0x00FFFF00 & rand());
+		hd.line2p(hd.width >> 1, hd.height >> 1, (rand() % hd.width), (rand() % hd.height), 0xFF000000 + 0x00FFFF00 & rand());
 	}
 	StretchDIBits(wnd->hdc, 0, 0, wnd->width, wnd->height, 0, 0, hd.width, hd.height, hd.handle, &wnd->buf_info, DIB_RGB_COLORS, SRCCOPY);
 
-	unsigned long dt = comp_mspf();
+	uint32_t dt = comp_mspf();
 	std::wstring s = L" ms to frame: " + std::to_wstring(dt) + L" mspf, speed: " + std::to_wstring(1000.0f/dt) + L" fps ";
 	TextOut(wnd->hdc, 0, 0, s.c_str(), s.length());
 }
 
 int WinMain(HINSTANCE hinst, HINSTANCE prev_hinst, LPSTR cmd_line, int show_cmd) {
 
-	hd.init(2*480, 2*320);
-	wnd.init(hinst, L"Graphics test", rend, 2*480, 2*320);
+	hd.init(3*480, 3*320);
+	wnd.init(hinst, L"Graphics test", rend, 3*480, 3*320);
 	wnd.init_draw_area(hd.width, hd.height);
 	wnd.show_window(show_cmd);
 
--- sh_win/handle.cpp
+++ sh_win/handle.cpp
@@ -23,7 +23,7 @@ void handle::clear(uint32_t hex) {
 	for (uint32_t i(width* height); i--; handle[i] = col3(hex));
 }
 
-void handle::line(int32_t cx, int32_t cy, int32_t dx, int32_t dy, uint32_t hex) {
+void handle::line(int32_t x, int32_t y, int32_t dx, int32_t dy, uint32_t hex) {
 #define __max__(a00, b00) (((a00) > (b00)) ? (a00) : (b00))
 #define __sign__(a00) ((a00)?(((a00)>0)?1:-1):0)
 #define __abs__(a00) (((a00)<0)?(-a00):(a00))
@@ -32,19 +32,19 @@ void handle::line(int32_t cx, int32_t cy, int32_t dx, int32_t dy, uint32_t hex) 
 	int8_t dir_x = __sign__(dx);
 	uint32_t del_x = __abs__(dx);
 	uint32_t erx = 0;
-	int32_t x = 0;
+	dx += x;
 
 	//y's defs
 	int8_t dir_y = __sign__(dy);
 	uint32_t del_y = __abs__(dy);
 	uint32_t ery = 0;
-	int32_t y = 0;
+	dy += y;
 
 	//max variation
 	uint32_t max_del = __max__(del_x, del_y);
-
+	
 	while (x != dx && y != dy) {
-		draw(cx + x, cy + y, hex);
+		draw(x, y, hex);
 
 		//x movement
 		erx += del_x;
@@ -66,48 +66,7 @@ void handle::line(int32_t cx, int32_t cy, int32_t dx, int32_t dy, uint32_t hex) 
 }
 
 void handle::line2p(int32_t x0, int32_t y0, int32_t x1, int32_t y1, uint32_t hex) {
-#define __max__(a00, b00) (((a00) > (b00)) ? (a00) : (b00))
-#define __sign__(a00) ((a00)?(((a00)>0)?1:-1):0)
-#define __abs__(a00) (((a00)<0)?(-a00):(a00))
-
-
-	//x's defs
-	x1 -= x0;
-	int8_t dir_x = __sign__(x1);
-	uint32_t del_x = __abs__(x1);
-	x1 += x0;
-	uint32_t erx = 0;
-
-	//y's defs
-	y1 -= y0;
-	int8_t dir_y = __sign__(y1);
-	uint32_t del_y = __abs__(y1);
-	y1 += y0;
-	uint32_t ery = 0;
-
-	//max variation
-	uint32_t max_del = __max__(del_x, del_y);
-
-	while (x0 != x1 && y0 != y1) {
-		draw(x0, y0, hex);
-
-		//x movement
-		erx += del_x;
-		if (erx >= max_del) {
-			erx -= max_del;
-			x0 += dir_x;
-		}
-
-		//y movement
-		ery += del_y;
-		if (ery >= max_del) {
-			ery -= max_del;
-			y0 += dir_y;
-		}
-	}
-#undef __max__
-#undef __sign__
-#undef __abs__
+	line(x0, y0, x1 - x0, y1 - y0, hex);
 }
 
 void handle::ring(int32_t cx, int32_t cy, uint32_t r, uint32_t hex) {
@@ -129,7 +88,6 @@ void handle::ring(int32_t cx, int32_t cy, uint32_t r, uint32_t hex) {
 		draw(cx - y, cy - x, hex);
 		
 		x++;
-		//O(x^6) power series approximation of sqrt(r^2 - x^2)
 		y = __y_from_x__(x, r);
 	}
 	if (x != y) return;
@@ -168,7 +126,6 @@ void handle::circ(int32_t cx, int32_t cy, uint32_t r, uint32_t hex) {
 			draw(cx - y, cy - n, hex);
 		}
 		x++;
-		//O(x^6) power series approximation of sqrt(r^2 - x^2)
 		y = __y_from_x__(x, r);
 	}
 	if (x != y) return;
@@ -186,27 +143,15 @@ void handle::circ(int32_t cx, int32_t cy, uint32_t r, uint32_t hex) {
 void handle::rect(int32_t cx, int32_t cy, int32_t dx, int32_t dy, uint32_t hex) {
 #define __sign__(a00) ((a00)?(((a00)>0)?1:-1):0)
 	int8_t dir_x = __sign__(dx);
+	dx += dir_x + cx;
 	int8_t dir_y = __sign__(dy);
-	dx += dir_x;
-	dy += dir_y;
-	for (int32_t x(0); x != dx; x += dir_x)
-		for (int32_t y(0); y != dy; y += dir_y)
-			draw(cx + x, cy + y, hex);
+	dy += dir_y + cy;
+	for (; cx != dx; cx += dir_x)
+		for (int32_t y(cy); y != dy; y += dir_y)
+			draw(cx, y, hex);
 #undef __sign__
 }
 
 void handle::rect2p(int32_t x0, int32_t y0, int32_t x1, int32_t y1, uint32_t hex) {
-#define __sign__(a00) ((a00)?(((a00)>0)?1:-1):0)
-	x1 -= x0;
-	int8_t dir_x = __sign__(x1);
-	x1 += x0 + dir_x;
-
-	y1 -= y0;
-	int8_t dir_y = __sign__(y1);
-	y1 += y0 + dir_y;
-
-	for (; x0 != x1; x0 += dir_x)
-		for (int32_t y(y0); y != y1; y += dir_y)
-			draw(x0, y, hex);
-#undef __sign__
+	rect(x0, y0, x1 - x0, y1 - y0, hex);
 }
